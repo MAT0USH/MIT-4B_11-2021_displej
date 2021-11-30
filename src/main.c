824c2b4be7c8c261c10224d3f993864519aa6d9a
MAT0USH <palenik.matous@seznam.cz>
2021-11-30 11:35:26 +0100

    ve škole

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "stm8s.h"
 #include "milis.h"
+#include "stdio.h"
+#include "stm8_hd44780.h"
 
 /*#include "delay.h"*/
-/*#include <stdio.h>*/
+#include <stdio.h>
 /*#include "../lib/uart.c"*/
 
 #define _ISOC99_SOURCE
@@ -10,19 +12,22 @@
 
 #define LED_PORT GPIOC
 #define LED_PIN  GPIO_PIN_5
-#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN);
-#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN);
-#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN);
+#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN)
+#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN)
+#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN)
 
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
 
 
-void init(void)
+void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+
+    lcd_init();
     init_milis();
 }
 
@@ -30,14 +35,23 @@ void init(void)
 int main(void)
 {
     uint32_t time = 0;
-    init();
+    uint16_t pocitadlo =0;
+    char text[16];
+
+    setup();
     /*init_uart();*/
+
+    lcd_gotoxy(0,0);
+    lcd_puts("ahoj");
 
     while (1) {
 
         if (milis() - time > 333 && BTN_PUSH) {
-            GPIO_WriteReverse(LED_PORT, LED_PIN);
+            LED_FLIP;
             time = milis();
+            lcd_gotoxy(0,0);
+            sprintf(text, "cislo %d", pocitadlo++);
+            lcd_puts(text);
         }
 
         /*LED_FLIP; */
